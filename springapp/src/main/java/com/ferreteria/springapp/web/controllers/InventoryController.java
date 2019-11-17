221afe10ee62d07daef660f8f995e1a614a16798
mferrero <mferrero@192.168.0.8>
2019-11-17 10:44:28 -0300

    Commit ultimo 17-11 10:44

--- springapp/src/main/java/com/ferreteria/springapp/web/controllers/InventoryController.java
+++ springapp/src/main/java/com/ferreteria/springapp/web/controllers/InventoryController.java
@@ -1,22 +1,35 @@
 package com.ferreteria.springapp.web.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ferreteria.springapp.business.services.ProductManager;
+
 @Controller
-public class HelloController {
+public class InventoryController {
 
     protected final Log logger = LogFactory.getLog(getClass());
+    
+    @Autowired
+    private ProductManager productManager;
 
     @RequestMapping(value = "/hello.htm")
     public ModelAndView handleRequest() {
         String now = (new Date()).toString();
         logger.info("Returning hello view with " + now);
-        return new ModelAndView("hello", "now", now);
+
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
+
+        return new ModelAndView("hello", "model", myModel);
     }
 }
--- springapp/src/test/java/com/ferreteria/springapp/web/controllers/InventoryControllerTests.java
+++ springapp/src/test/java/com/ferreteria/springapp/web/controllers/InventoryControllerTests.java
@@ -2,6 +2,8 @@ package com.ferreteria.springapp.web.controllers;
 
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +18,19 @@ import com.ferreteria.springapp.web.SpringappWebConfig;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {SpringappBusinessConfig.class, SpringappWebConfig.class})
 @WebAppConfiguration
-public class HelloControllerTests {
+public class InventoryControllerTests {
 
     @Autowired
-    private HelloController controller;
+    private InventoryController controller;
 
     @Test
     public void testHandleRequestView() {	
         ModelAndView modelAndView = controller.handleRequest();		
         assertEquals("hello", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
-        String nowValue = (String) modelAndView.getModel().get("now");
+        @SuppressWarnings("unchecked")
+        Map<String, Object> modelMap = (Map<String, Object>) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
         assertNotNull(nowValue);
     }
 }
